fd18de40bb6a524d4d3897388f7fad1b96809cff
islam.ar <islam.ahmed@approcks.com>
2022-05-08 07:37:23 +0200

    GetCarImagesRepository Unit Test

---
+++ app/src/test/java/com/move/mvisample/data/remote/repositories/GetCarImagesRepositoryImplTest.kt
@@ -0,0 +1,37 @@
+package com.move.mvisample.data.remote.repositories
+
+import com.move.mvisample.data.remote.AppService
+import com.move.mvisample.domain.entites.CarResponse
+import com.move.mvisample.presentation.viewmodel.main.MainResults
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.whenever
+import retrofit2.Response
+
+class GetCarImagesRepositoryImplTest {
+
+    lateinit var repository: GetCarImagesRepositoryImpl
+
+    @Mock
+    lateinit var appService: AppService
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.openMocks(this)
+        repository = GetCarImagesRepositoryImpl(appService)
+    }
+
+    @Test
+    fun successStatue_getCars() = runBlocking {
+        val params = "111"
+        val networkResponse = Response.success(CarResponse(listOf()))
+
+        whenever(appService.getCars(params)).thenReturn(networkResponse)
+
+        assertEquals(networkResponse, repository.getCars(params))
+    }
+}
