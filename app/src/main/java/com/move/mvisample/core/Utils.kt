2bdd0e8ffe51005a7e436f28ada5e057014a4246
islam.ar <islam.ahmed@approcks.com>
2022-05-08 09:54:37 +0200

    update check network function

--- app/src/main/java/com/move/mvisample/core/Utils.kt
+++ app/src/main/java/com/move/mvisample/core/Utils.kt
@@ -2,16 +2,14 @@ package com.move.mvisample.core
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 
 object Utils {
-
-    fun isOnline(context: Context): Boolean { //TODO update method
-        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE)
-                as ConnectivityManager
-        val networkInfo = connectivityManager.activeNetworkInfo
-        return networkInfo != null && networkInfo.isConnected
-        /*
-         val connectivityManager = context.connectivityManager
+    @Suppress("DEPRECATION")
+    fun isOnline(context: Context): Boolean {
+         val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE)
+                 as ConnectivityManager
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             val network = connectivityManager.activeNetwork ?: return false
@@ -26,30 +24,11 @@ object Utils {
                 else -> false
             }
         } else {
-            @Suppress("DEPRECATION") val networkInfo =
+           val networkInfo =
                 connectivityManager.activeNetworkInfo ?: return false
-            @Suppress("DEPRECATION")
             return networkInfo.isConnected
         }
-         */
-        /*val connectivityManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        if (connectivityManager != null) {
-            val capabilities =
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-                }
-            if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
-                return true
-            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
-                Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
-                return true
-            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
-                Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
-                return true
-            }
-        }
-        return false*/
+
     }
 
 }
